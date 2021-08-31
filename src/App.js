e414b1d13a67bb0cddca4db5e9708a24ae949610
Matheus Delmondes <mdelmondes@icloud.com>
2021-08-31 01:17:11 -0300

    Finalizado layout, rolagem lateral dos filmes e etc

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
+      //pegando a lista dos filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //pegando o destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
   }, []);
 
+  useEffect (() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  })
+
   return (
     <div className="page">
+      <Header black={blackHeader} />      
+      {featuredData && 
+        <FeaturedMovie item={featuredData}/>
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           <div>
@@ -25,6 +56,16 @@ export default () => {
           </div>
         ))}
       </section>
+
+      <footer>
+        Site clone feito pelo Matheus D.
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"/>
+        </div>
+      }
     </div>
   )
 }
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css';
+import { NavigateBefore } from '@material-ui/icons';
+import { NavigateNext } from '@material-ui/icons';
 
 export default ({title, itens}) => {
+    const [scrollX, setScrollX] = useState(0);
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let totalList = itens.results.length * 150;
+        if((window.innerWidth - totalList) > x){
+            x = (window.innerWidth - totalList) - 60;
+        }
+        setScrollX(x)
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+            <div className="movieRow--arrowLeft" onClick={handleLeftArrow}>
+                <NavigateBefore style={{fontSize: 50}} />
+            </div>
+            <div className="movieRow--arrowRight" onClick={handleRightArrow}>
+                <NavigateNext style={{fontSize: 50}} />
+            </div>
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: itens.results.length * 150
+                }}>
                     {itens.results.length > 0 && itens.results.map((item,key) => (
                         <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
